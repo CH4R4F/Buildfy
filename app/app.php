745c578c04a9d1419007c095c46bd726cdb62460
charaf <developermarghin@gmail.com>
2022-06-09 08:53:13 +0100

    add simple authentication system

--- app/app.php
+++ app/app.php
@@ -2,6 +2,9 @@
   // require config file
   require_once('config/config.php');
 
+  // require helpers file
+  require_once('helpers/redirect.php');
+
   // autoload classes
   spl_autoload_register(function($className) {
     require_once('lib/' . $className . '.php');
